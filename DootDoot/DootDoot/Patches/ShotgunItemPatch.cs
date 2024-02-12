5cb8b246bb7f50f0bf16c7aea9f2bf06957818f6
Cory Bergquist <cory3bergquist@gmail.com>
2024-02-12 00:34:13 -0500

    Basic doot audio

---
+++ DootDoot/DootDoot/Patches/ShotgunItemPatch.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace DootDoot.Patches
+{
+    [HarmonyPatch(typeof(ShotgunItem))]
+    internal class ShotgunItemPatch
+    {
+        [HarmonyPatch("ShootGun")]
+        [HarmonyPrefix]
+        static void shootGunPatch(ShotgunItem __instance)
+        {
+            if (__instance.isHeldByEnemy)
+            {
+                __instance.gunShootSFX = DootDootModBase.DootDootSFX;
+            }
+        }
+    }
+}
---
+++ DootDoot/DootDoot/Plugin.cs
@@ -0,0 +1,50 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using DootDoot.Patches;
+using HarmonyLib;
+using UnityEngine;
+
+namespace DootDoot
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class DootDootModBase : BaseUnityPlugin
+    {
+        private const string modGUID = "8bitRedux.DootDoot";
+        private const string modName = "DootDoot";
+        private const string modVersion = "0.1.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static DootDootModBase Instance;
+
+        internal static AssetBundle Bundle;
+        internal static AudioClip[] DootDootSFX;
+        internal static ManualLogSource MLS;
+
+        void Awake()
+        {
+            if(Instance == null)
+            {
+                Instance = this;
+            }
+
+            MLS = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+            MLS.LogInfo("Doot Doot initializing...");
+
+            harmony.PatchAll(typeof(DootDootModBase));
+            harmony.PatchAll(typeof(ShotgunItemPatch));
+
+            string folderLocation = Instance.Info.Location.TrimEnd("DootDoot.dll".ToCharArray());
+            Bundle = AssetBundle.LoadFromFile(folderLocation + "doot-doot");
+            if(Bundle != null)
+            {
+                DootDootSFX = Bundle.LoadAllAssets<AudioClip>();
+                MLS.LogInfo("Successfully loaded doot-doot bundle");
+            }
+            else
+            {
+                MLS.LogError("Failed to load doot-doot bundle");
+            }
+        }
+    }
+}
